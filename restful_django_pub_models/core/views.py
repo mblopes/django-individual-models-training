6d28c296604cd28f6a702cf33953edd55647eac7
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-02-11 12:06:27 -0300

    Created views

--- restful_django_pub_models/core/views.py
+++ restful_django_pub_models/core/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CategorySerializer, ProductSerializer, \
+                         CustomerSerializer, SportSerializer, TeamSerializer
+from .models import Category, Product, Customer, Sport, Team
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('id')
+    serializer_class = CategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('id')
+    serializer_class = CustomerSerializer
+
+
+class SportViewSet(viewsets.ModelViewSet):
+    queryset = Sport.objects.all().order_by('sport_name')
+    serializer_class = SportSerializer
+
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all().order_by('id')
+    serializer_class = TeamSerializer
